38ec3caf542753d157c0a1f5d23fd17f5d17783a
srinidhiy <syerragu@usc.edu>
2023-09-05 00:17:06 -0700

    update middleware

--- middleware.ts
+++ middleware.ts
@@ -6,7 +6,7 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
     publicRoutes: ['/', '/api/webhook/clerk'],
     ignoredRoutes: ['/api/webhook/clerk'],
-    debug: true,
+    // debug: true,
 });
 
 export const config = {
